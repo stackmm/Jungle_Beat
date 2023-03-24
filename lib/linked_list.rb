32aee8d3684ded71cc9b6d210e0a90310d42cf58
Steven Stackhouse <stackmm@gmail.com>
2023-03-23 20:41:38 -0600

    add linked_list.rb and linked_list.spec

---
+++ lib/linked_list.rb
@@ -0,0 +1,16 @@
+class LinkedList
+
+  def initialize()
+
+  end
+
+  def append(node)
+  end
+
+  def count
+  end
+
+  def to_string
+  end
+  
+end
---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require './lib/node'
+require './lib/linked_list'
+
+RSpec.describe LinkedList do
+
+
+end
