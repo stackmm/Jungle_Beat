07bb314418ba78dfa1fedacad6d069e8154cf8c9
Steven Stackhouse <stackmm@gmail.com>
2023-03-23 21:02:18 -0600

    add append method to linked_list.rb

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,16 +1,35 @@
 class LinkedList
 
+  attr_reader :head
   def initialize()
-
+    @head = nil
   end
 
-  def append(node)
+  def append(data)
+    @head = Node.new(data)
   end
 
   def count
+    if self.is_empty?
+      0
+    else
+      count = 1
+      node = @head
+      while (node = node.next_node)
+        node = node.next_node
+        count +=1
+      end
+      count
+    end
   end
 
   def to_string
+    node = @head
+    p node
+
+    while (node = node.next_node)
+      p node
+    end
   end
-  
+
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -3,6 +3,24 @@ require './lib/node'
 require './lib/linked_list'
 
 RSpec.describe LinkedList do
+  it "exists" do
+    list = LinkedList.new
+    expect(list).to be_an_instance_of(LinkedList)
+  end
 
+  it "has readable attributes" do
+    list = LinkedList.new
+    expect(list.head).to eq(nil)
+  end
+
+  it "can append a new piece of data" do
+    list = LinkedList.new
+    list.append("doop")
+    expect(list.head.data).to eq("doop")
+    list.append(0)
+    expect(list.head.data).to eq(0)
+    list.append(false)
+    expect(list.head.data).to eq(false)
+  end
 
 end
