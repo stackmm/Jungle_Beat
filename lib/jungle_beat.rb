abb7172d7919ac89094ceccca0946610cbaf0b97
Steven Stackhouse <stackmm@gmail.com>
2023-03-25 13:52:55 -0600

    add jungle_beat.rb and test file

---
+++ lib/jungle_beat.rb
@@ -0,0 +1,6 @@
+class JungleBeat
+  attr_reader
+  def initialize()
+    @list = LinkedList.new
+  end
+end
---
+++ spec/jungle_beat_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require './lib/node'
+require './lib/linked_list'
+require './lib/jungle_beat'
+
+RSpec.describe JungleBeat do
+  it "exists" do
+  end
+
+end
